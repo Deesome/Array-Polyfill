999340dab3c1d2143138810188a1ff5e11beee84
Deepak Yadav <deepak160895@gmail.com>
2025-02-13 16:45:05 +0530

    Update Flat Method

--- flat.js
+++ flat.js
@@ -1,20 +1,23 @@
 //flat()
 // return new arrat with all sub-array elements upto specified depth, depth as input
 
-Array.prototype.myFlat = function(depth){
-    if(depth===undefined) depth=1;
-    let arrayCounter=0;
-    const result = []
-
-    for(let i=0;i<this.length;i++){
-        if(isArray(this[i])){
-            
-        }
-        arrayCounter++
-         result.push(...this[i])
-
+Array.prototype.myFlat = function(depth=1){
+    // NEED to understand Recursion
+    let result = []
+    if(depth===Infinity){
+        depth = Number.MAX_SAFE_INTEGER
     }
+    for(let i = 0; i<this.length; i++){
+
+        if(Array.isArray(this[i]) && depth>0){
+            result = result.concat(this[i].myFlat(depth-1))
+        }else{
+            result.push(this[i])
+        }
+    } 
 
     return result
 }
 
+const arr = [1,[2,6,8,[1,2],10]]
+console.log(arr.myFlat(2))
