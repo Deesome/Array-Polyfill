6ed228f6b5da924dca7188fc56c37c41e5110422
Ravi Bhardwaj <121806214+ravirajbhardwaj@users.noreply.github.com>
2025-02-16 18:12:46 +0530

    fix: handle additional edge cases in myForEach

--- forEach.js
+++ forEach.js
@@ -4,7 +4,9 @@
 const arr = [1,2,3,4,5]
 
 Array.prototype.myForEach = function(userFunction){
-
+    if (typeof userFunction !== 'function') {
+        throw new TypeError(`${typeof userFunction} ${userFunction} is not a function`);
+    }
     for(let i=0;i<this.length;i++){
         userFunction(this[i],i)
 
